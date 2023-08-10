1ec7190e654432a215d42788830d86922e90d8d2
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-08-10 16:43:54 +0900

    Add markdown recognition feature

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import "@/app/[id]/post.css";
+import Post from '@/app/posts/니파 유데미 학습일지 3주차.mdx';
 
 export default function Page({ params }: { params: { id: string } }) {
+
     return <>
-        <main>My Post: {params.id}</main>
+        <main>
+            <Post />
+        </main>
     </>
 }
