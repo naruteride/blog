150362845ac19bf1a3d7fe20d1af01fb4e6f4792
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-08-10 15:29:06 +0900

    Modify to dynamically display post content from data file

---
+++ src/app/PostData.tsx
@@ -0,0 +1,13 @@
+export const PostData: {
+    title: string;
+    date: string;
+    summary: string;
+    tags: string[];
+}[] = [
+        {
+            title: "니파 유데미 학습일지 3주차",
+            date: "2023-08-10",
+            summary: "니파는 신이고 나는 무능하다.",
+            tags: ["nipa", "학습일지"],
+        }
+    ]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,26 @@
 import Link from 'next/link';
-import "./article.css"
+import "./posts.css"
+import { PostData } from "@/app/PostData";
 
 export default function Home() {
   return <>
     <main>
       <h3>글</h3>
 
-      <Link href="./제목제목">
+      {PostData.map(data => <Link href={`./${data.title}`}>
         <article>
-          <h1>제목제목</h1>
-          <time>2023-08-10</time>
+          <h1>{data.title}</h1>
+          <time>{data.date}</time>
           <summary>
-            요약된 글 or 앞부분 글
+            {data.summary}
           </summary>
           <div className="tags">
-            <Link className="tag" href="./">
-              #nipa
-            </Link>
-            <Link className="tag" href="./">
-              #nipa
-            </Link>
-            <Link className="tag" href="./">
-              #nipa
-            </Link>
+            {data.tags.map(tag => <span className="tag">
+              #{tag}
+            </span>)}
           </div>
         </article>
-      </Link>
-
-      <Link href="./제목제목">
-        <article>
-          <h1>제목제목</h1>
-          <time>2023-08-10</time>
-          <summary>
-            요약된 글 or 앞부분 글
-          </summary>
-          <div className="tags">
-            <Link className="tag" href="./">
-              #nipa
-            </Link>
-            <Link className="tag" href="./">
-              #nipa
-            </Link>
-            <Link className="tag" href="./">
-              #nipa
-            </Link>
-          </div>
-        </article>
-      </Link>
+      </Link>)}
     </main>
   </>
 }
