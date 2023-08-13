a65aa4325b146daaa57387a3e7c0d47898f9cd45
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-08-14 00:31:03 +0900

    Add a type at mdx post & Add a alert

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,8 +1,10 @@
 import "@/app/[id]/post.css";
+import { MDXContent } from "mdx/types";
 
 export default async function Page({ params }: { params: { id: string } }) {
-    const { default: Post } = await import(`@/app/posts/${decodeURI(params.id)}.mdx`);
+    const { default: Post }: {default: MDXContent} = await import(`@/app/posts/${decodeURI(params.id)}.mdx`);
     console.log(params.id);
+    alert(params.id);
 
     return (
         <main className="post">
