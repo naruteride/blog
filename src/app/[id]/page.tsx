04826bb9eeb02322b716f3e49e683e5e57f1bcee
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-08-14 00:45:37 +0900

    Fix a bug where imports were not working when accessing the page directly through the address bar

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,8 +2,17 @@ import "@/app/[id]/post.css";
 import { MDXContent } from "mdx/types";
 
 export default async function Page({ params }: { params: { id: string } }) {
-    const { default: Post }: { default: MDXContent } = await import(`@/app/posts/${decodeURI(params.id)}.mdx`);
-    console.log(params.id);
+    let Post: MDXContent;
+
+    try {
+        Post = (await import(`@/app/posts/${params.id}.mdx`)).default;
+    } catch (error) {
+        try {
+            Post = (await import(`@/app/posts/${decodeURI(params.id)}.mdx`)).default;
+        } catch (error) {
+            Post = () => <div>서버 상태 500</div>;
+        }
+    }
 
     return (
         <main className="post">
