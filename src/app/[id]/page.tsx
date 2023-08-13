5acfd0d2a7e68efeb6f8697ac67a258daa4fc029
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-08-14 00:19:36 +0900

    Fix a background image bug & Add a console log

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,6 +2,7 @@ import "@/app/[id]/post.css";
 
 export default async function Page({ params }: { params: { id: string } }) {
     const { default: Post } = await import(`@/app/posts/${decodeURI(params.id)}.mdx`);
+    console.log(params.id);
 
     return (
         <main className="post">
