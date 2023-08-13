e93ed8ab679bf17afbb44f451511acc613fbf59d
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-08-14 01:08:12 +0900

    Remove try...cahtch

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,15 +4,15 @@ import { MDXContent } from "mdx/types";
 export default async function Page({ params }: { params: { id: string } }) {
     let Post: MDXContent | React.FC;
 
-    try {
-        Post = (await import(`@/app/posts/${params.id}.mdx`)).default;
-    } catch (error) {
-        try {
+    // try {
+    //     Post = (await import(`@/app/posts/${params.id}.mdx`)).default;
+    // } catch (error) {
+    //     try {
             Post = (await import(`@/app/posts/${decodeURI(params.id)}.mdx`)).default;
-        } catch (error) {
-            Post = () => <div>서버 상태 500</div>;
-        }
-    }
+    //     } catch (error) {
+    //         Post = () => <div>서버 상태 500</div>;
+    //     }
+    // }
 
     return (
         <main className="post">
