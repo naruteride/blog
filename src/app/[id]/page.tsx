301d7623353ee5baef6caa4d150d57593d192c4e
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-08-14 00:33:44 +0900

    Remove a alert

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,9 +2,8 @@ import "@/app/[id]/post.css";
 import { MDXContent } from "mdx/types";
 
 export default async function Page({ params }: { params: { id: string } }) {
-    const { default: Post }: {default: MDXContent} = await import(`@/app/posts/${decodeURI(params.id)}.mdx`);
+    const { default: Post }: { default: MDXContent } = await import(`@/app/posts/${decodeURI(params.id)}.mdx`);
     console.log(params.id);
-    alert(params.id);
 
     return (
         <main className="post">
