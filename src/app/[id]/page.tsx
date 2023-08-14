7677ac7b60a5dc3b32b964f36ebf7834b2e39fc9
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-08-15 05:16:11 +0900

    Delete comments

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -5,13 +5,9 @@ export default async function Page({ params }: { params: { id: string } }) {
     let Post: MDXContent /*| React.FC*/;
 
     try {
-        Post = (await import(`@/app/posts/${params.id}.mdx`)).default;
+        Post = (await import(`@/app/posts/${decodeURI(params.id)}.mdx`)).default;
     } catch (error) {
-        // try {
-            Post = (await import(`@/app/posts/${decodeURI(params.id)}.mdx`)).default;
-        // } catch (error) {
-        //     Post = () => <div>서버 상태 500</div>;
-        // }
+        Post = (await import(`@/app/posts/${params.id}.mdx`)).default;
     }
 
     return (
